ff637b15be8d7ce3361a88b632c69b1e6a72b0f5
Andy Tran <foolmonkey99@gmail.com>
2020-11-30 22:17:42 -0600

    Add hero banner to About page

--- app/src/views/About.js
+++ app/src/views/About.js
@@ -1,13 +1,19 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import { Link } from "react-router-dom";
 
 const About = (props) => {
   return (
-    <main>
-      <h1>About Us</h1>
-      <h2>
-        We are Cloud Cafe, dedicated to providing you with quality ingredients,
-        brews, and service!
-      </h2>
+    <main className="about">
+      <div className="hero">
+        <img src="/images/about.jpg"></img>
+        <div>
+          <h1>About Us</h1>
+          <h2>
+            We are Cloud Cafe, dedicated to providing you with quality
+            ingredients, brews, and service!
+          </h2>
+        </div>
+      </div>
     </main>
   );
 };
